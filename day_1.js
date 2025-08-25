1c751224269231c583e80e2814e31df0fe63d155
ravina34 <krabina514@gmail.com>
2025-08-25 13:32:21 +0530

    project added

---
+++ day_1.js
@@ -0,0 +1,13 @@
+// Student object
+const student = {
+  name: "Ravi",
+  age: 20,
+  marks: 85,
+  course: "B.Tech CSE"
+};
+
+// Print student details
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Marks:", student.marks);
+console.log("Course:", student.course);
